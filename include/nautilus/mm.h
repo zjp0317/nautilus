9d83900f00845b44a34db887502caa169b308c64
zjp0317 <jiz150@pitt.edu>
2019-11-19 16:45:38 -0500

    disable dynamic for now

--- include/nautilus/mm.h
+++ include/nautilus/mm.h
@@ -95,7 +95,7 @@ struct mem_region;
 #define REGULAR_MEM_SIZE    (PISCES_MEM_UNIT) // 128MB for regular usage: runtime, apps, etc.
 #define MINIMUM_MEM_SIZE    (INTERNAL_MEM_SIZE + REGULAR_MEM_SIZE) // At least INTERNAL_MEM_SIZE + 128MB for booting nautilus-pisces
 
-#define NAUT_CONFIG_PISCES_DYNAMIC 1 // TODO move this to menuconfig
+//#define NAUT_CONFIG_PISCES_DYNAMIC 1 // TODO move this to menuconfig
 
 #ifdef NAUT_CONFIG_PISCES_DYNAMIC
 #define PREFETCH_THRESHOLD  (0.75)  // send async request for more memory if current mem usage >= 75%    
