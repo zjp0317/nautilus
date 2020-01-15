e762d3b799586aead1782032a5478a392b938a2f
zjp0317 <jiz150@pitt.edu>
2020-01-15 13:47:22 -0800

    fix larson

--- src/test/larson.c
+++ src/test/larson.c
@@ -286,8 +286,8 @@ handle_larson (char * buf, void * priv)
     min_threads = max_threads;
 
 // zjp
-    blkp = (char**)malloc(sizeof(char*) * ((max_threads+1)*chperthread )) ;
-    blksize = (int*)malloc(sizeof(int) * ((max_threads+1)*chperthread )) ;
+    blkp = (char**)malloc(sizeof(char*) * (num_chunks*chperthread )) ;
+    blksize = (int*)malloc(sizeof(int) * ((num_chunks*chperthread )) ;
 
     printf ("sleep = %ld, min = %d, max = %d, per thread = %d, num rounds = %d, seed = %d, max_threads = %d, min_threads = %d\n",
             sleep_cnt, min_size, max_size, chperthread, num_rounds, seed, max_threads, min_threads);
